fc1b2680a87557eebb47852c2ae062f7e9d761d6
gauravghid <gauravglory28@gmail.com>
2018-05-22 11:21:47 +0530

    InvoiceContacts APi Updated

--- routes/invoicecontacts.js
+++ routes/invoicecontacts.js
@@ -6,9 +6,9 @@ var router = express.Router();
 router.get('/:country', function(req, res, next) {
   console.log(req.params.country);
   var country = req.params.country;
-  var result = _.find(invoiceContactsData,{"country": country});
+  var result = _.find(invoiceContactsData,{"country": country.toLowerCase()});
   if(result){
-  	res.status(201).json(result);
+  	res.status(201).json({"country":country,"email": result["email"]});
   } else {
   	res.status(200).json({"Error Code": "12003","Error Message":"Invoice submission contact details not available for input data."});
   }  
